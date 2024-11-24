26b8fb98dc296345963269781833754e35327aa7
Abhinavpreet Singh Arora <aroraabhinavpreetsingh@gmail.com>
2024-11-25 00:31:32 +0530

    comments setting and background image of quiz

--- index.js
+++
@@ -1,6 +0,0 @@
-const toggler = document.getElementById('toggler');
-const navLinks = document.getElementById('nav-links');
-
-toggler.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
